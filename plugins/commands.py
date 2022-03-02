88ca85c79822e28f092f3729eb997f3d3509dc35
nrx77 <97866445+nrx77@users.noreply.github.com>
2022-03-03 06:08:34 +0800

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -19,7 +19,7 @@ logger = logging.getLogger(__name__)
 BATCH_FILES = {}
 
 @Client.on_message(filters.command("") & filters.incoming & ~filters.edited)
-async def st(client, message):
+async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
@@ -30,7 +30,7 @@ async def st(client, message):
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
-        #await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
+        await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
@@ -53,7 +53,7 @@ async def st(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=script.SeTART_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
